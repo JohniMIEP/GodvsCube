0116b888b3275e3b8607465d2d5bbdd5bd1daf7d
John Tran <johntran69444@outlook.de>
2022-11-24 22:32:27 +0100

    lmao

---
+++ main.py
@@ -0,0 +1,10 @@
+
+import src.GameBoard
+
+def main():
+    game = GameBoard()
+    print(game.getWidth())
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/GameBoard.py
@@ -0,0 +1,11 @@
+
+class GameBoard():
+    def __init__():
+        self.WIDTH = 1000
+        self.HEIGHT = 1000
+        
+    def getWidth():
+        return this.WIDTH
+    
+    def getHeight():
+        return this.HEIGHT
